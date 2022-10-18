828e0d0b8801cd362ddc218123b3abe4d7554b21
Temuujin Erdenebileg <102133245+temuujin2@users.noreply.github.com>
2022-10-18 11:42:57 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <h1>Bogino</h1>
+        <form>
+          <input type="text" placeholder='https://www.web-huudas.mn'></input>
+          <button>БОГИНОСГОХ</button>
+        </form>
+      </header>
+    </div>
+  );
+}
+
+export default App;
